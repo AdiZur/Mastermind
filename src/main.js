6732cec22acbb21c9ad97687283197e1499fe356
DAVID_OREN <david525246@gmail.com>
2024-09-16 22:44:51 +0300

    Update the game loop

--- src/main.js
+++ src/main.js
@@ -1,27 +1,45 @@
-const consts = require ('./consts');
-const prompt = require('prompt-sync')({ sigint: true });
+const consts = require('./consts');
+const prompt = require('prompt-sync')();
 
 function gameLoop() {
-    // const lottery = 'פונקציה שמבצעת הגרלה על הצבעים'
-  
-    let answer = prompt(`Welcome! In front of you are six colors: ${consts.OPTIONS}.
+  let answer = '';
+  const previous = [];
+  let count = 0;
+
+  const lottery =
+    /* 'פונקציה שמבצעת הגרלה על הצבעים'*/
+
+    console.log(`Welcome! In front of you are six colors: ${consts.OPTIONS}.
         of which I have chosen four colors,
         and you must find the colors I have chosen
         as well as their exact location in the smallest number of attempts 
-        and no more than ten. please try: `);
+        and no more than ten. Please enter numbers separated by a space. To stop the game click "ctrl + c".
+        please try: `);
 
-    // פונקציה שבודקת את התשובות
+  // פונקציה שבודקת את התשובות
 
-    let count = 1
-    while (answer !== lottery && count <= consts.MAX_CHANCES){
-        console.log(`you selected: ${answer}. and you got ${/*פונקציה שמחזירה בול או פגיעה*/}`);
-        
-        answer = prompt(`you have ${consts.MAX_CHANCES - count} attempts left. please try again: `)
-        count++
-    }
-    if (answer === lottery){
-        return console.log(`congratulations you won in ${count} attempts`)
-        }
-    return console.log (`failed! sorry, you tried ${count} times out of ${consts.MAX_CHANCES} attempts. The correct answer was: ${lottery}`)
+  while (answer !== lottery && count <= consts.MAX_CHANCES) {
+    count++;
+
+    const pars = 'פונקציה שמפרסרת את קלט המשתמש';
+
+    const result = 'פונקציה שבודקת את התשובה של המשתמש';
+
+    previous.push(pars);
+
+    console.log(
+      `Your previous attempts ${previous}. your choice now is: ${pars}. and you got ${result}`
+    );
+
+    answer = prompt(
+      `you have ${consts.MAX_CHANCES - count} attempts left. please try again: `
+    );
+  }
+  if (answer === lottery) {
+    return console.log(`congratulations you won in ${count} attempts`);
+  }
+  return console.log(
+    `failed! sorry, you tried ${count} times out of ${consts.MAX_CHANCES} attempts. The correct answer was: ${lottery}`
+  );
 }
 console.log(gameLoop());
