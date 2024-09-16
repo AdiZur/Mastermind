76fd8d9f864bcfb3b72c9ffde2674134899a7a58
DAVID_OREN <david525246@gmail.com>
2024-09-16 17:27:39 +0300

    Creating the code loop

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-console.log('hello world');
+const consts = require ('./consts');
+const prompt = require('prompt-sync')({ sigint: true });
+
+function gameLoop() {
+    // const lottery = 'פונקציה שמבצעת הגרלה על הצבעים'
+  
+    let answer = prompt(`Welcome! In front of you are six colors: ${consts.OPTIONS}.
+        of which I have chosen four colors,
+        and you must find the colors I have chosen
+        as well as their exact location in the smallest number of attempts 
+        and no more than ten. please try: `);
+
+    // פונקציה שבודקת את התשובות
+
+    let count = 1
+    while (answer !== lottery && count <= consts.MAX_CHANCES){
+        console.log(`you selected: ${answer}. and you got ${/*פונקציה שמחזירה בול או פגיעה*/}`);
+        
+        answer = prompt(`you have ${consts.MAX_CHANCES - count} attempts left. please try again: `)
+        count++
+    }
+    if (answer === lottery){
+        return console.log(`congratulations you won in ${count} attempts`)
+        }
+    return console.log (`failed! sorry, you tried ${count} times out of ${consts.MAX_CHANCES} attempts. The correct answer was: ${lottery}`)
+}
+console.log(gameLoop());
