abc5a0241a017b8107d8db14b49af09c556645f3
AdiZur <adizur9@gmail.com>
2024-09-17 21:34:57 +0300

    adding params notations and description to the function

--- src/colors.js
+++ src/colors.js
@@ -1,4 +1,7 @@
-
+/**
+ * The function gets an array of strings and prints them.
+ * @param {[string]} colors 
+ */
 function printColors(colors) {
     colors.forEach((color, index) => {
         console.log(`${index + 1}: ${color}`);
