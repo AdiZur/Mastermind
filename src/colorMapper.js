9fef0606419e52d3c84cc621a618935cb9f5b8b7
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-09-18 14:29:23 +0300

    fix comments. split convertUserGuessToArray to 3 helper functions : removeRedundantSpaces, validateUserGuess, mapUserInputToColors.
    add to consts file OPTION_TO_GUESS
    rename OPTIONS to COLORS_OPTIONS

---
+++ src/colorMapper.js
@@ -0,0 +1,58 @@
+/***
+ * This module provides functions to generate a random array of colors to guess,
+ * and to convert user input from a string to an array of colors.
+ *
+ * Functions:
+ * - getColorsToGuess: Generates a random array of colors to guess.
+ * - convertUserGuessToArray: Converts user input from a string to an array of colors.
+ */
+
+const {OPTIONS_TO_GUESS, COLORS_OPTIONS: colorOptions} = require('consts');
+
+/**
+ * the function use array of string - colorOptions and return random array of new options
+ * @return {string[]} colorsToGuess
+ */
+module.exports = function getColorsToGuess() {
+    return new Array(OPTIONS_TO_GUESS).fill(0)
+        .map(() => colorOptions[Math.floor(Math.random() * colorOptions.length)]);
+}
+
+/**
+ * the function convert the user input from string to array og colors
+ * @param {string} userInput
+ * @return {string[]} colorsToGuess
+ */
+module.exports = function convertUserGuessToArray(userInput) {
+    const numbersInput = removeRedundantSpaces(userInput).split(' ');
+    const validInput = validateUserGuess(numbersInput);
+
+    return validInput ? mapUserInputToColors(numbersInput) : null;
+}
+
+/**
+ * @param {string} userInput
+ * @return {string} fixUserInput
+ */
+function removeRedundantSpaces(userInput) {
+    return userInput.trim().replace(/\s+/g, ' ');
+}
+
+/**
+ * return true if the input is valid, else return false.
+ * @param {string[]} numbersInput
+ * @return {boolean} validInput
+ */
+function validateUserGuess(numbersInput) {
+    return numbersInput.length !== OPTIONS_TO_GUESS ||
+        (numbersInput.every(num => !Number.isNaN(num) &&
+            !(0 <= num && num < colorOptions.length)));
+}
+
+/**
+ * @param {string[]} userInput
+ * @return {string[]} fixUserInputArray
+ */
+function mapUserInputToColors(userInput) {
+    return userInput.map(num => colorOptions[parseInt(num)])
+}
--- src/consts.js
+++ src/consts.js
@@ -1,3 +1,5 @@
-module.exports.OPTIONS = ['Red', 'Yellow', 'Blue', 'Green', 'Black', 'White'];
+module.exports.COLORS_OPTIONS = ['Red', 'Yellow', 'Blue', 'Green', 'Black', 'White'];
 
 module.exports.MAX_CHANCES = 10;
+
+module.exports.OPTIONS_TO_GUESS = 4;
