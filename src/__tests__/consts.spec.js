bd25eb9b23f5c51928fd04299ba396770bd2b7ad
adimalka14 <122941157+adimalka14@users.noreply.github.com>
2024-09-19 16:32:21 +0300

    finish tests add jest.config file for colorMapper and consts. fix bugs in colorMapper : 1. exports function bug 2. change || to && in validateInput function

---
+++ src/__tests__/consts.spec.js
@@ -0,0 +1,13 @@
+const { COLORS_OPTIONS, MAX_CHANCES, OPTIONS_TO_GUESS } = require('../consts');
+
+test('should have correct color options', () => {
+    expect(COLORS_OPTIONS).toEqual(['Red', 'Yellow', 'Blue', 'Green', 'Black', 'White']);
+});
+
+test('should have correct max chances', () => {
+    expect(MAX_CHANCES).toBe(10);
+});
+
+test('should have correct options to guess', () => {
+    expect(OPTIONS_TO_GUESS).toBe(4);
+});
